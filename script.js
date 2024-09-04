f560742ac466c48f944596c5bb16bcf287ae4b02
Nikhil1120 <NIKHILT1120@GMAIL.COM>
2024-09-04 10:27:39 +0530

    change

--- script.js
+++ script.js
@@ -123,6 +123,13 @@ const music = [
         "imageLink": "https://www.koimoi.com/wp-content/new-galleries/2022/01/pushpa-to-cross-250-crore-mark-today-001.jpg",
         "link": "https://od.lk/s/MTJfMjE2ODY0NDBf/Eyy%20Bidda%20Idhi%20Naa%20Adaa.mp3",
         "year": "2021"
+    },
+    {
+        "title": "Pushpa",
+        "artist": "Allu Arjun",
+        "imageLink": "https://www.koimoi.com/wp-content/new-galleries/2022/01/pushpa-to-cross-250-crore-mark-today-001.jpg",
+        "link": "https://od.lk/s/MTJfMjE2ODY0NDBf/Eyy%20Bidda%20Idhi%20Naa%20Adaa.mp3",
+        "year": "2021"
     }
 ]
 
