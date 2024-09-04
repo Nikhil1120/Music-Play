ce2a4e26d3cc8db82a69ca05f970ccdf2f7af46a
Nikhil1120 <NIKHILT1120@GMAIL.COM>
2024-09-04 10:17:45 +0530

    hi

---
+++ script.js
@@ -0,0 +1,210 @@
+const music = [
+    {
+        "title": "Arya",
+        "artist": "Allu Arjun",
+        "imageLink": "https://thetelugufilmnagar.com/storage/2021/05/WhatsApp-Image-2021-05-07-at-18.31.20.jpeg ",
+        "link": "https://od.lk/s/MTJfMjE2ODY1OTNf/Feel%20My%20Love.mp3",
+        "year": "2004",
+    },
+    {
+        "title": "Bunny",
+        "artist": "Allu Arjun",
+        "imageLink": "https://upload.wikimedia.org/wikipedia/en/3/36/Bunny_Telugu_poster.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI1OTRf/Maaro%20Maaro-SenSongsMp3.Com.mp3",
+        "year": "2005",
+    },
+    {
+        "title": "Happy",
+        "artist": "Allu Arjun",
+        "imageLink": "https://cinemachaat.com/wp-content/uploads/2011/01/happy.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI1ODdf/%5BiSongs.info%5D%2002%20-%20Happy.mp3",
+        "year": "2006",
+    },
+
+    {
+        "title": " Desamuduru",
+        "artist": "Allu Arjun",
+        "imageLink": "https://m.media-amazon.com/images/S/pv-target-images/e68f0353f0b076d2b18c52d1dab21296eaacb9d867889c049387cecb33189f91.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI1ODNf/%5BiSongs.info%5D%2004%20-%20Chal%20Chal.mp3",
+        "year": "2007",
+    },
+    {
+        "title": " Parugu",
+        "artist": "Allu Arjun",
+        "imageLink": "https://m.media-amazon.com/images/M/MV5BMmZkYmE5NzQtMGM1MS00ZDEwLTljMDMtNjc2YTRmOTE2NDUwXkEyXkFqcGdeQXVyNjE2NTgxOTE@._V1_.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI1NzJf/%5BiSongs.info%5D%2004%20-%20Chal%20Chal.mp3",
+        "year": "2008",
+    },
+    {
+        "title": " Araya-2",
+        "artist": "Allu Arjun",
+        "imageLink": "https://m.media-amazon.com/images/M/MV5BMmY4OTk1MTctYTM1My00Y2IyLWJlMmUtNzUyOThhYzM4YmNkXkEyXkFqcGdeQXVyNjA0MTQ0MjI@._V1_.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI1NjZf/%5BiSongs.info%5D%2002%20-%20Uppenantha.mp3",
+        "year": "2009",
+    },
+    {
+        "title": "Varudu",
+        "artist": "Allu Arjun",
+        "imageLink": "https://m.media-amazon.com/images/M/MV5BOGU0ZDc3NDItYWJkMS00OWY2LThlOTQtNzNkNmZjZjJkOTBkXkEyXkFqcGdeQXVyNjM1NzkxNTM@._V1_.jpg",
+        "link": "https://od.lk/s/MTJfMjE2ODY0NDBf/Eyy%20Bidda%20Idhi%20Naa%20Adaa.mp3",
+        "year": "2010",
+    },
+    {
+        "title": "Badrinath",
+        "artist": "Allu Arjun",
+        "imageLink": "https://m.media-amazon.com/images/M/MV5BOGI2ODY0NmEtZWNlMi00MjZkLThhNTctYzE0MzY1MzY3ZGU5XkEyXkFqcGdeQXVyNDY5MTUyNjU@._V1_.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI1NDFf/In%20The%20Night-SenSongsMp3.Com.mp3",
+        "year": "2011",
+    },
+    {
+        "title": "Julayi",
+        "artist": "Allu Arjun",
+        "imageLink": "https://www.idlebrain.com/images4/wp-20julayi1366.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI1MzFf/O%20Madhu-SenSongsMp3.Com.mp3",
+        "year": "2012",
+    },
+    {
+        "title": "Iddarammayilatho",
+        "artist": "Allu Arjun",
+        "imageLink": "https://m.media-amazon.com/images/M/MV5BZDNiYmEzNGEtYmFmYy00ZDcwLTlhYjYtNjA2MGI2MDQwOGQ0XkEyXkFqcGdeQXVyNjkwOTg4MTA@._V1_.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI1MjJf/Run%20Run-SenSongsMp3.Com.mp3",
+        "year": "2013",
+    },
+    {
+        "title": "Race Gurram",
+        "artist": "Allu Arjun",
+        "imageLink": "https://c.saavncdn.com/483/Race-Gurram-Telugu-2014-20231017181522-500x500.jpg",
+        "link": "https://od.lk/s/MTJfMjE2ODY2NDFf/Sweety.mp3",
+        "year": "2014"
+    },
+    {
+        "title": "Son-of-Satyamurthi",
+        "artist": "Allu Arjun",
+        "imageLink": "https://content.tupaki.com/twdata/2015/0415/news/Movie-Review--S-o-Satyamurthy--By-Hapra--113.jpg",
+        "link": "https://od.lk/s/MTJfMjE2ODY2MTZf/One%20%26%20Two%20%26%20Three%20-%20SenSongsMp3.Co.mp3",
+        "year": "2015"
+    },
+    {
+        "title": "Sarrainodu",
+        "artist": "Allu Arjun",
+        "imageLink": "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/sarrainodu-et00038117-17-11-2020-02-06-39.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI0OTNf/%5BiSongs.info%5D%2005%20-%20Telusaa%20Telusaa.mp3",
+        "year": "2016"
+    },
+    {
+        "title": "DJ Duvvada Jagannatham",
+        "artist": "Allu Arjun",
+        "imageLink": "https://m.media-amazon.com/images/M/MV5BZmM3MjMyZmMtNDYzZi00MDc4LTgwZGItZGU0YzlkMTE1YjRkXkEyXkFqcGdeQXVyNjAwODkyNDM@._V1_.jpg",
+        "link": "https://od.lk/s/MTJfMjE2OTI0ODFf/Seeti%20Maar-SenSongsMp3.Co.mp3",
+        "year": "2017"
+    },
+    {
+        "title": " Naa Peru Surya Na Illu India",
+        "artist": "Allu Arjun",
+        "imageLink": "https://www.telugu360.com/wp-content/uploads/2018/03/Naa-Peru-Surya-1.jpg",
+        "link": "https://od.lk/s/MTJfMjE2ODY2MjBf/Sainika%20-%20SenSongsMp3.Co.mp3",
+        "year": "2018"
+    },
+
+
+
+    {
+        "title": "Ala Vaikunthapurramuloo",
+        "artist": "Allu Arjun",
+        "imageLink": "https://www.telugu360.com/wp-content/uploads/2019/11/maxresdefault-21.jpg",
+        "link": "https://od.lk/s/MTJfMjE2ODY2NTVf/Daddy.mp3",
+        "year": "2020"
+    },
+
+
+    {
+        "title": "Pushpa",
+        "artist": "Allu Arjun",
+        "imageLink": "https://www.koimoi.com/wp-content/new-galleries/2022/01/pushpa-to-cross-250-crore-mark-today-001.jpg",
+        "link": "https://od.lk/s/MTJfMjE2ODY0NDBf/Eyy%20Bidda%20Idhi%20Naa%20Adaa.mp3",
+        "year": "2021"
+    }
+]
+
+const mainDiv = document.getElementById('main');
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
+// Create exit button
+const exitButton = document.createElement('button');
+exitButton.textContent = 'Exit';
+exitButton.style.display = 'none';
+exitButton.style.marginTop='50px';
+exitButton.style.backgroundColor='blue';
+exitButton.style.color='white';
+exitButton.style.fontSize='15px';
+
+mainDiv.parentNode.appendChild(exitButton);
+
+// Function to display music data
+function displayData(data) {
+    mainDiv.innerHTML = '';
+    data.forEach((ele) => {
+        const musicDiv = document.createElement('div');
+        musicDiv.classList.add('music');
+        musicDiv.dataset.title = ele.title;
+
+        const image = document.createElement('img');
+        image.classList.add('img-photo');
+        image.src = ele.imageLink;
+        image.alt = ele.title;
+
+        const title = document.createElement('h2');
+        title.textContent = ele.title;
+
+        const year = document.createElement('h4');
+        year.textContent = ele.year;
+
+        const artistName = document.createElement('p');
+        artistName.textContent = ele.artist;
+
+        const link = document.createElement('a');
+        link.textContent = "Favorite Song";
+        link.href = ele.link;
+        link.target = "_blank";
+
+        musicDiv.append(image, title, artistName, year, link);
+        mainDiv.appendChild(musicDiv);
+    });
+}
+// Initial display
+displayData(music);
+
+
+// Function to search music data
+function searchMusic(searchTerm) {
+    const filteredData = music.filter((ele) =>
+        ele.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    displayData(filteredData);
+    exitButton.style.display = 'block';
+    searchInput.value = searchTerm;
+}
+// Exit button event listener
+exitButton.addEventListener('click', resetSearch);
+
+// Function to reset search
+function resetSearch() {
+    searchInput.value = '';
+    displayData(music);
+    exitButton.style.display = 'none';
+}
+
+
+
+// Search event listener
+searchButton.addEventListener('click', () => {
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm) {
+        searchMusic(searchTerm);
+    } else {
+        resetSearch();
+    }
+});
+
+
